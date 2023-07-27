94ef12fb678366bda60408d6c608bc141f714aef
Oluwaniyi66 <Oluwaniyi66.oluwaniyiropo11@gmail.com>
2023-07-27 15:58:31 +0100

    footer, youtube api,overall style

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('unregister/<int:registration_id>/', views.unregister, name='unregister'),
     path('logout', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
+    path('youtube', views.youtube, name = 'youtube'),
+
 ]
