549d3739e33f0b687be918a9bde83ded5bd3dbaa
Abdullah-Alholiel <A.alholaiel@gmail.com>
2023-07-27 19:37:30 +0100

    fixing views.py and trying youtube API

--- WebApp/views.py
+++ WebApp/views.py
@@ -43,3 +43,39 @@ def unregister(request, registration_id):
     registration = Registration.objects.get(id=registration_id)
     registration.delete()
     return redirect('list_modules')
+def courses(request):
+    courses = Group.objects.all()
+    return render(request, 'WebApp/courses.html', {'courses': courses})
+
+def youtube(request):
+    if request.method == 'POST':
+        form = YoutubeForm(request.POST)
+        text = request.POST['text']
+        video = VideosSearch(text, limit = 5)
+        result_list = []
+        for i in video.result()['result']:
+            result_dict = {
+                'input': text,
+                'title': i['title'],
+                'duration': i['duration'],
+                'thumbnail': i['thumbnails'][0]['url'],
+                'channel' : i['channel']['name'],
+                'link': i['link'],
+                'views': i['viewCount']['short'],
+                'published':i['publishedTime'],
+            }
+            desc = ''
+            if i['descriptionSnippet']:
+                for j in i['descriptionSnippet']:
+                    desc += j['text']
+                result_dict['description'] = desc
+            result_list.append(result_dict)
+        context = {
+            'form': form,
+            'results': result_list
+        }
+        return render(request, 'WebApp/youtube.html', context)
+    else: 
+        form = YoutubeForm()
+    context = {'form': form}
+    return render(request, "WebApp/youtube.html", context)
