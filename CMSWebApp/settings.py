e9e42dd403e08f003632f5d6c27b9cae3df14106
Abdullah-Alholiel <A.alholaiel@gmail.com>
2023-08-09 16:27:12 +0000

    initalizing app service

--- CMSWebApp/settings.py
+++ CMSWebApp/settings.py
@@ -134,21 +134,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = "static/"
-# MEDIA_URL = "/media/"
+STATIC_URL = "static/css/"
+MEDIA_URL = "/media/"
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 AZURE_SA_NAME = os.environ['AZURE_SA_NAME']
 AZURE_SA_KEY = os.environ['AZURE_SA_KEY']
 
-DEFAULT_FILE_STORAGE = 'WebApp.storages.AzureMediaStorage'
-STATICFILES_STORAGE = 'WebApp.storages.AzureStaticStorage'
+# DEFAULT_FILE_STORAGE = 'WebApp.storages.AzureMediaStorage'
+# STATICFILES_STORAGE = 'WebApp.storages.AzureStaticStorage'
 
-STATIC_URL = f'https://{AZURE_SA_NAME}.blob.core.windows.net/static/static'
-Media_URL = f'https://{AZURE_SA_KEY}.blob.core.windows.net/media'
+# STATIC_URL = f'https://{AZURE_SA_NAME}.blob.core.windows.net/static/static/'
+# Media_URL = f'https://{AZURE_SA_KEY}.blob.core.windows.net/media/'
 
 
 # Default primary key field type
