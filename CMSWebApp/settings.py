e2fd97aa6927af4ce0a0185480b13f1d852b2bf2
Abdullah-Alholiel <A.alholaiel@gmail.com>
2023-08-10 16:03:49 +0000

    fixed settings

--- CMSWebApp/settings.py
+++ CMSWebApp/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = "static/css/"
+STATIC_URL = "static/css/"
 # MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -147,8 +147,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 AZURE_SA_NAME = os.environ['AZURE_SA_NAME']
 AZURE_SA_KEY = os.environ['AZURE_SA_KEY']
 
-DEFAULT_FILE_STORAGE = 'WebApp.storages.AzureMediaStorage'
-STATICFILES_STORAGE = 'WebApp.storages.AzureStaticStorage'
+DEFAULT_FILE_STORAGE = 'CMSWebApp.storages.AzureMediaStorage'
+# STATICFILES_STORAGE = 'WebApp.storages.AzureStaticStorage'
 
 STATIC_URL = f'https://{AZURE_SA_NAME}.blob.core.windows.net/static/static/'
 Media_URL = f'https://{AZURE_SA_KEY}.blob.core.windows.net/media/'
