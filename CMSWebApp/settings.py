b3311ac75dfd5afa0262ddac8f8f4488bdf6b64a
Abdullah-Alholiel <A.alholaiel@gmail.com>
2023-08-10 15:47:52 +0000

    whitenoise added

--- CMSWebApp/settings.py
+++ CMSWebApp/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AZURE_SA_NAME = os.environ['AZURE_SA_NAME']
 AZURE_SA_KEY = os.environ['AZURE_SA_KEY']
